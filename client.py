9fd1d2dd0eec1e0d332ddd60af4b9536f66eeabe
Falcon-I <76063025+Falcon-I@users.noreply.github.com>
2021-05-08 11:14:00 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,28 +1,60 @@
 import time, socket, sys
-server = input("server's IP :")
-time.sleep(1)
+import tkinter.scrolledtext as st
+import tkinter as tk
+
+server = '192.168.29.137'
 soc = socket.socket()
 shost = socket.gethostname()
 ip = socket.gethostbyname(shost)
 server_host = server
 name = input("your name :")
 port = 1234
-print('Trying to connect to the server: {}, ({})'.format(server_host, port))
 time.sleep(1)
 soc.connect((server_host, port))
-print("Connected...")
+print("Conneteced")
 soc.send(name.encode())
-server_name = soc.recv(1024)
-server_name = server_name.decode()
-print('{} has joined...'.format(server_name))
-print("Enter 'stop_chat_server' to exit.")
-while True:
-    message = soc.recv(1024)
-    message = message.decode()
-    print(server_name, ">", message)
-    message = input('                                                                     you' + "> ")
-    soc.send(message.encode())
-    if message == "stop_chat_server":
-        message1 = "bye........(SIGNAL LOST)"
-        soc.send(message1.encode())
-        break
+server_name = "Chaitu"
+
+
+
+def sends():
+    try:
+        my_message = message_box.get()
+        soc.send(my_message.encode())
+        message_show.insert(tk.INSERT, name + " : " + my_message + ".\n")
+    except:
+        message_show.insert(tk.INSERT, "Signal lost. Can't send your message.\n")
+
+
+
+
+
+def receive():
+    try:
+        client_message = soc.recv(1024)
+        fyclient_message = client_message.decode()
+        message_show.insert(tk.INSERT, server_name + " : " + fyclient_message + ".\n")
+    except:
+        message_show.insert(tk.INSERT, "Signal lost.....\n")
+
+
+
+root = tk.Tk()
+root.title("C-chat")
+screenwidth = root.winfo_screenwidth()
+screenheight = root.winfo_screenheight()
+root.geometry("%dx%d" % (screenwidth, screenheight))
+root['background'] = '#856ff8'
+message_show = st.ScrolledText(width = 80,height = 30)
+message_show.grid(column = 0, padx = 300, pady = 100)
+message_box = tk.Entry()
+message_box.place(x=300, y=650, width=600, height=40)
+send = tk.Button(text="Send", width=10, height=2, command=sends)
+send.place(x=910, y=650)
+receive = tk.Button(text="Receive", width=10, height=2, command=receive)
+receive.place(x=1290, y=0)
+
+
+
+
+root.mainloop()
